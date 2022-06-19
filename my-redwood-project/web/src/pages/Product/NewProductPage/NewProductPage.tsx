8ef57d3209da510f549db57146fbb5dd43f89894
hossam1231 <hossamsulleman@gmail.com>
2022-06-19 16:44:55 +0100

    Init

--- my-redwood-project/web/src/pages/Product/NewProductPage/NewProductPage.tsx
+++
@@ -1,7 +0,0 @@
-import NewProduct from 'src/components/Product/NewProduct'
-
-const NewProductPage = () => {
-  return <NewProduct />
-}
-
-export default NewProductPage
--- my-redwood-project/web/src/pages/Product/ProductsPage/ProductsPage.tsx
+++
@@ -1,7 +0,0 @@
-import ProductsCell from 'src/components/Product/ProductsCell'
-
-const ProductsPage = () => {
-  return <ProductsCell />
-}
-
-export default ProductsPage

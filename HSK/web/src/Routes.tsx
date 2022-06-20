99ddd014b8108198c2f6bc7e8d31898159a422a4
hossam1231 <hossamsulleman@gmail.com>
2022-06-20 18:52:50 +0100

    Footer layout completed

--- HSK/web/src/Routes.tsx
+++ HSK/web/src/Routes.tsx
@@ -7,11 +7,15 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Set } from '@redwoodjs/router'
+import FooterLayout from 'src/layouts/FooterLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Set wrap={FooterLayout}>
+        <Route path="/" page={HomePage} name="home" />
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )

aa59e894383c0395f6890639cd6b4524b94be002
Hossam1231 <Hossamsulleman@gmail.com>
2022-07-07 16:45:58 +0100

    init

--- HSK/web/src/components/Shop/Shop.stories.tsx
+++
@@ -1,7 +0,0 @@
-import Shop from './Shop'
-
-export const generated = () => {
-  return <Shop />
-}
-
-export default { title: 'Components/Shop' }
--- HSK/web/src/pages/ShopPage/ShopPage.stories.tsx
+++
@@ -1,7 +0,0 @@
-import ShopPage from './ShopPage'
-
-export const generated = () => {
-  return <ShopPage />
-}
-
-export default { title: 'Pages/ShopPage' }

856d517eb9b46c1fc905a4b187d646edbb167835
Hossam1231 <Hossamsulleman@gmail.com>
2022-07-16 16:43:59 +0100

    new start here

--- App.jsx
+++ App.jsx
@@ -1,34 +1,14 @@
-// react
-import React from "react";
-
-// redux
-import { Provider as ReduxProvider } from "react-redux";
-import { Store } from "./services/redux/store";
-
-// nativebase
-import { NativeBaseProvider } from "native-base";
-
-// react native
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
-// screens
-import { LoadingScreen } from "./screens/loading/LoadingScreen";
+import { Box, NativeBaseProvider } from 'native-base'
+import React from 'react'
 
 const App = () => {
-	return (
-		<ReduxProvider store={Store}>
-			{/* <PersistGate loading={false} persistor={persister}> */}
-			<NativeBaseProvider>
-				<SafeAreaProvider>
-					<LoadingScreen
-						spinnerColor={"black"}
-						textColor={"black"}
-						accessibilityLabel={"loading cache"}
-					/>
-				</SafeAreaProvider>
-			</NativeBaseProvider>
-		</ReduxProvider>
-	);
-};
+  return (
+	<NativeBaseProvider>
+		<Box w="20" h="20" bg="black">
 
-export default App;
+		</Box>
+	</NativeBaseProvider>
+  )
+}
+
+export default App

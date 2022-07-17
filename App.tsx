f93ef195e757438462cdd32e2c069a104ab6e7fe
Hossam1231 <Hossamsulleman@gmail.com>
2022-07-17 21:33:55 +0100

    firebase authentication added

--- App.tsx
+++ App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { ThemeProvider } from 'react-native-elements';
+import './config/firebase';
+import RootNavigation from './navigation';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <ThemeProvider>
+      <RootNavigation />
+    </ThemeProvider>
+  );
 }

44584c89a0e7ac1f2d6e02b094662305c50845d1
Hossam1231 <Hossamsulleman@gmail.com>
2022-07-26 05:11:37 +0100

    added styiing o priciing page

--- web/src/pages/PricingPage/PricingPage.js
+++ web/src/pages/PricingPage/PricingPage.js
@@ -1,40 +1,38 @@
-import styled from 'styled-components'
-
-import ProductsCell from 'src/components/ProductsCell'
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
 
 const PricingPage = () => {
   return (
-    <Wrapper>
-      <article>
-        <h2>Super Tokens</h2>
-        <Description>
-          These are single-use tokens. Great for emergencies and gifts.
-        </Description>
-        <ProductsCell />
-      </article>
+    <>
+      <MetaTags title="Pricing" description="Pricing page" />
 
-      <article>
-        <h2>Super Subs</h2>
-        <Description>
-          Monthly subscriptions to superpowers. Great for the career
-          supervillain, hero or parent.
-        </Description>
-        <ProductsCell type={'recurring'} />
-      </article>
-    </Wrapper>
+      <div style={{ alignItems: 'center' }}>
+        <div style={{ display: 'flex' }} className="max-w-xs">
+          <div>
+            <p>Plans</p>
+          </div>
+          <div>
+            <p>Bespoke</p>
+          </div>
+        </div>
+        <div>
+          <p>Chose to get started</p>
+        </div>
+        <button className="group block max-w-xs bg-white mx-auto rounded-lg p-6 shadow-lg hover:bg-violet-600 focus:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 shadow-lg space-y-3">
+          <div className="flex items-center space-x-3">
+            {/* <svg class="h-6 w-6 stroke-sky-500 group-hover:stroke-white" fill="none" viewBox="0 0 24 24"><!-- ... --></svg> */}
+            <h3 className="text-slate-900 group-hover:text-white text-sm font-semibold  group-focus:text-white text-sm font-semibold  ">
+              New project
+            </h3>
+          </div>
+          <p className="text-slate-500 group-hover:text-white text-sm group-focus:text-white text-sm">
+            Create a new project from a variety of starting templates.
+          </p>
+        </button>
+        <p>Please enter the Wallet ID or Destination email</p>
+      </div>
+    </>
   )
 }
 
 export default PricingPage
-
-// Styles
-
-const Wrapper = styled.main`
-  display: flex;
-  flex-direction: column;
-  gap: calc(var(--padding) * 2);
-`
-
-const Description = styled.p`
-  margin-bottom: var(--padding);
-`
--- web/src/pages/PricingPage/PricingPage.stories.js
+++ web/src/pages/PricingPage/PricingPage.stories.js
@@ -1,21 +1,7 @@
-import CartProvider from 'src/components/CartProvider'
-import MainLayout from 'src/layouts/MainLayout'
-
 import PricingPage from './PricingPage'
 
 export const generated = () => {
   return <PricingPage />
 }
 
-export default {
-  title: 'Pages/PricingPage',
-  decorators: [
-    (Story) => (
-      <CartProvider>
-        <MainLayout>
-          <Story />
-        </MainLayout>
-      </CartProvider>
-    ),
-  ],
-}
+export default { title: 'Pages/PricingPage' }

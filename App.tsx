761b9654f7d85f2b6bf6a1ff6b6f78bcf15e1e31
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-08-07 10:59:17 +0100

    swithing branch

--- App.tsx
+++ App.tsx
@@ -1,30 +1,34 @@
 import {React, useState, useEffect }from 'react';
+
+import {SafeAreaProvider }from 'react-native';
 import { ThemeProvider } from 'react-native-elements';
 import './config/firebase';
 import RootNavigation from './navigation';
-import { NativeBaseProvider, Center, Spinner } from "native-base";
+import { NativeBaseProvider, Center, Spinner,StatusBar} from "native-base";
 import useCachedResources from "./hooks/useCachedResources";
 import './index.css'
+import LoadingScreen from './screens/loading/LoadingScreen'
 
 export default function App() {
+
 let isLoadingComplete = useCachedResources();
 
-
-if (!isLoadingComplete) {
-  return (
-    <NativeBaseProvider>
-<Center flex="1">
-  <Spinner></Spinner>
-</Center>
-    </NativeBaseProvider>
-  );
-} else {
-  return (
-    <ThemeProvider>
-      <NativeBaseProvider>
-              <RootNavigation />
-      </NativeBaseProvider>
-    </ThemeProvider>
-  );
-}
+	if (!isLoadingComplete) {
+		return (
+			<NativeBaseProvider>
+				<LoadingScreen />
+			</NativeBaseProvider>
+		);
+	} else {
+		return (
+      <ThemeProvider>
+			<NativeBaseProvider>
+				<SafeAreaProvider>
+			      <RootNavigation />
+					<StatusBar />
+				</SafeAreaProvider>
+			</NativeBaseProvider>
+          </ThemeProvider>
+		);
+	}
 }

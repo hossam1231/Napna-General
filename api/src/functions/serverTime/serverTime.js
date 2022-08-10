57add6570fbd602b4d894bbeef7ab508d2316b03
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-08-10 07:51:04 +0100

    push

--- api/src/functions/serverTime/serverTime.js
+++ api/src/functions/serverTime/serverTime.js
@@ -17,14 +17,12 @@ import { logger } from 'src/lib/logger'
  * function, and execution environment.
  */
 export const handler = async (event, context) => {
+  logger.info('Invoked serverTime function')
+
+  export const handler = async (event, context) => {
   return {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-
-    body: JSON.stringify({
-      data: 'getMerchantId function',
-    }),
+    body: new Date()
   }
+}
 }

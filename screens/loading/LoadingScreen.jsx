bc6241ee6c5ed27d44509a8b40ba88bc904b9208
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-08-12 22:15:38 +0100

    build

--- screens/loading/LoadingScreen.jsx
+++ screens/loading/LoadingScreen.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { Spinner, Center, Heading, HStack } from "native-base";
 
-const LoadingScreen = () => {
-	return (
-		<Center flex="1">
-			{/* napna logo */}
-			<Loading />
-		</Center>
-	);
-};
+export default function LoadingScreen() {
+  return (
+    <Center flex="1">
+      {/* napna logo */}
+      <Loading />
+    </Center>
+  );
+}
 
 export const Loading = () => {
-	return (
-		<HStack space={2} justifyContent="center">
-			<Spinner accessibilityLabel="Loading posts" />
-			<Heading color="black" fontSize="md">
-				Loading
-			</Heading>
-		</HStack>
-	);
+  return (
+    <HStack space={2} justifyContent="center">
+      <Spinner accessibilityLabel="Loading posts" />
+      <Heading color="black" fontSize="md">
+        Loading
+      </Heading>
+    </HStack>
+  );
 };
-
-export default LoadingScreen;

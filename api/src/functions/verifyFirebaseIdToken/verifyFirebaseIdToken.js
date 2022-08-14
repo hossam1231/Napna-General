5c71a997d03e7b003b81e32d71a71bddfda764e5
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-08-14 13:00:11 +0100

    push

--- api/src/functions/verifyFirebaseIdToken/verifyFirebaseIdToken.js
+++ api/src/functions/verifyFirebaseIdToken/verifyFirebaseIdToken.js
@@ -1,5 +1,21 @@
-
 import { logger } from 'src/lib/logger'
+
+/**
+ * The handler function is your code that processes http request events.
+ * You can use return and throw to send a response or error, respectively.
+ *
+ * Important: When deployed, a custom serverless function is an open API endpoint and
+ * is your responsibility to secure appropriately.
+ *
+ * @see {@link https://redwoodjs.com/docs/serverless-functions#security-considerations|Serverless Function Considerations}
+ * in the RedwoodJS documentation for more information.
+ *
+ * @typedef { import('aws-lambda').APIGatewayEvent } APIGatewayEvent
+ * @typedef { import('aws-lambda').Context } Context
+ * @param { APIGatewayEvent } event - an object which contains information from the invoker.
+ * @param { Context } context - contains information about the invocation,
+ * function, and execution environment.
+ */
 
 let HEADERS = {
   'Access-Control-Allow-Headers':
@@ -14,11 +30,11 @@ HEADERS['Vary'] = 'Origin'
 HEADERS['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
 
 export const handler = async (event, context) => {
-
-  logger.info('Invoked verifyFirebaseIdToken function')
   return {
     statusCode: 200,
     HEADERS,
-    body: JSON.stringify({ hello: 'world' })
+    body: JSON.stringify({
+      data: '${name} function',
+    }),
   }
 }

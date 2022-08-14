f19d3c4748ca99855f0a50c6bb0fd0ae190ff9f5
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-08-14 14:29:03 +0100

    try

--- api/src/functions/verifyFirebaseIdToken/verifyFirebaseIdToken.js
+++ api/src/functions/verifyFirebaseIdToken/verifyFirebaseIdToken.js
@@ -19,15 +19,13 @@ import { logger } from 'src/lib/logger'
 
 let HEADERS = {
   'Access-Control-Allow-Headers':
-    'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin',
+    'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin:*',
   'Content-Type': 'application/json', //optional
-  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, FETCH',
   'Access-Control-Max-Age': '8640',
 }
 
-HEADERS['Access-Control-Allow-Origin'] = '*'
-HEADERS['Vary'] = 'Origin'
-HEADERS['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, FETCH'
+
 
 export const handler = async (event, context) => {
   const url = require('url')

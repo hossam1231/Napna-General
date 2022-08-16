899951798efa3eb72167a469575c9c4ae362bfe4
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-08-16 09:24:53 +0100

    save for now

---
+++ api/src/functions/postLogin/postLogin.js
@@ -0,0 +1,33 @@
+import { logger } from 'src/lib/logger'
+
+let HEADERS = {
+  'Access-Control-Allow-Headers':
+    'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin',
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Max-Age': '8640',
+  'Access-Control-Allow-Origin': '*',
+}
+
+export const handler = async (event, context) => {
+  //all outward facing api should expect a token to validate before continuing
+  logger.info('Invoked postLogin function')
+
+  const { token, site } = event.queryStringParameters
+
+  if (token && site) {
+
+
+}
+
+
+
+
+  return {
+    statusCode: 200,
+    HEADERS,
+    body: JSON.stringify({
+      data: 'postLogin function',
+    }),
+  }
+}

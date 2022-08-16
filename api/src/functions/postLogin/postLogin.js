026d4994a837d5c197cd08ac3ee652fb17a5671d
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-08-16 13:34:37 +0100

    push

---
+++ api/src/functions/postLogin/postLogin.js
@@ -0,0 +1,45 @@
+import { logger } from "src/lib/logger";
+import fetch from "node-fetch";
+
+logger.info("Invoked postLogin function");
+let HEADERS = {
+  "Access-Control-Allow-Headers":
+    "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Max-Age": "8640",
+  "Access-Control-Allow-Origin": "*",
+};
+
+let res;
+
+export const handler = async (event, context) => {
+  const { token, site } = event.queryStringParameters;
+  //all outward facing api should expect a token to validate before continuing
+
+  async function postLoginBackend() {
+    let APIURL =
+      "http://napna.co.uk/.netlify/functions/verifyFirebaseIdToken?token=REPLACE_TOKEN";
+    APIURL = APIURL.replace("REPLACE_TOKEN", token);
+    console.log(APIURL, "sending out request");
+    res = await fetch(APIURL);
+    if (res.ok) {
+      const data = await res.json();
+      return data;
+    }
+  }
+
+  if (site == "Merchant") {
+    if (token) {
+      res = await postLoginBackend();
+    }
+  }
+
+  return {
+    statusCode: 200,
+    HEADERS,
+    body: JSON.stringify({
+      data: [token, site, res],
+    }),
+  };
+};

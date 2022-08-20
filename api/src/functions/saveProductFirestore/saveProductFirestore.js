25634e9f4f68fd32862ca0ebf59c2389d8088aff
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-08-20 16:38:23 +0100

    push

--- api/src/functions/saveProductFirestore/saveProductFirestore.js
+++ api/src/functions/saveProductFirestore/saveProductFirestore.js
@@ -39,9 +39,9 @@ export const handler = async (event, context) => {
 
   const db = getFirestore(firebaseApp)
 
-  const citiesRef = db.collection('cities')
+  const productsRef = db.collection('products')
 
-  await citiesRef.doc('SF').set({
+  let res = await productsRef.doc('SF').set({
     name: 'San Francisco',
     state: 'CA',
     country: 'USA',
@@ -49,42 +49,10 @@ export const handler = async (event, context) => {
     population: 860000,
     regions: ['west_coast', 'norcal'],
   })
-  await citiesRef.doc('LA').set({
-    name: 'Los Angeles',
-    state: 'CA',
-    country: 'USA',
-    capital: false,
-    population: 3900000,
-    regions: ['west_coast', 'socal'],
-  })
-  await citiesRef.doc('DC').set({
-    name: 'Washington, D.C.',
-    state: null,
-    country: 'USA',
-    capital: true,
-    population: 680000,
-    regions: ['east_coast'],
-  })
-  await citiesRef.doc('TOK').set({
-    name: 'Tokyo',
-    state: null,
-    country: 'Japan',
-    capital: true,
-    population: 9000000,
-    regions: ['kanto', 'honshu'],
-  })
-  await citiesRef.doc('BJ').set({
-    name: 'Beijing',
-    state: null,
-    country: 'China',
-    capital: true,
-    population: 21500000,
-    regions: ['jingjinji', 'hebei'],
-  })
 
   return {
     statusCode: 200,
     HEADERS,
-    body: JSON.stringify(citiesRef),
+    body: JSON.stringify(res),
   }
 }

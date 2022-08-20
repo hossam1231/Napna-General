c370260de704516f65921e0915633916794f3441
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-08-20 15:30:03 +0100

    moving

--- screens/Menu/MenuScreen.tsx
+++ screens/Menu/MenuScreen.tsx
@@ -14,20 +14,23 @@ import {
   Pressable,
 } from "native-base";
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
-
+import { useNavigation } from "@react-navigation/native";
 export default function MenuScreen() {
-  return <Example />;
+  return <AppDrawer></AppDrawer>;
 }
 
 function AppDrawer() {
+  const navigation = useNavigation();
   const icons = [
     {
       name: "bolt",
       bg: "amber.600",
+      route: "Home",
     },
     {
       name: "build",
       bg: "emerald.600",
+      route: "Product",
     },
     {
       name: "cloud",
@@ -104,36 +107,34 @@ function AppDrawer() {
   ];
   return (
     <FlatList
+      flex="1"
       numColumns={4}
-      m={"-8px"}
       data={icons}
       renderItem={({ item }) => {
         return (
-          <IconButton
-            m={"8px"}
-            borderRadius="full"
-            bg={item.bg}
-            variant="solid"
-            p="3"
-            icon={
-              <Icon
-                color="white"
-                name={item.name}
-                as={MaterialIcons}
-                size="sm"
-              />
-            }
-          />
+          <>
+            <IconButton
+              m={"8px"}
+              borderRadius="full"
+              bg={item.bg}
+              variant="solid"
+              onPress={() => {
+                navigation.navigate(item.route);
+              }}
+              p="3"
+              icon={
+                <Icon
+                  color="white"
+                  name={item.name}
+                  as={MaterialIcons}
+                  size="sm"
+                />
+              }
+            />
+            <Text>{item.name}</Text>
+          </>
         );
       }}
     />
-  );
-}
-
-function Example() {
-  return (
-    <Box alignItems="center" flex={1}>
-      <AppDrawer />
-    </Box>
   );
 }

440149ad67759905a5375fe4db44c289a8d3b15a
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-08-21 08:07:13 +0100

    pushh

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,21 @@ const functions = require("firebase-functions");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+
+  const db = getFirestore()
+
+  const productsRef = db.collection('products')
+
+  productsRef.doc('SF').set({
+    name: 'San Francisco',
+    state: 'CA',
+    country: 'USA',
+    capital: false,
+    population: 860000,
+    regions: ['west_coast', 'norcal'],
+  })
+
+  response.send(res);
+});

3c6e0d280759351c16f301f6b22b9e10e03c877d
hossam1231 <93336567+hossam1231@users.noreply.github.com>
2022-09-10 01:00:43 +0100

    step two now set up the partner sign up lol

---
+++ web/src/assets/FindAssets.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FindAssets = () => {
+  return (
+    <div>
+      {/* nothiung speciual jut a way for my keyboard shortcuts to findf the place */}
+    </div>
+  )
+}
+
+export default FindAssets
--- web/src/components/Auth/AuthenticationTools.tsx
+++ web/src/components/Auth/AuthenticationTools.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 
 import FaceIcon from '@mui/icons-material/Face'
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded'
+import GoogleIcon from '@mui/icons-material/Google'
+import { Grid, TextField } from '@mui/material'
 import Backdrop from '@mui/material/Backdrop'
 import Box from '@mui/material/Box'
 import Fade from '@mui/material/Fade'
@@ -13,14 +16,37 @@ const style = {
   position: 'absolute' as const,
   top: '50%',
   left: '50%',
+  width: '500px',
+  height: '500px',
   transform: 'translate(-50%, -50%)',
+}
+
+const inputStyle = {
+  textField: {
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    paddingBottom: 0,
+    marginTop: 0,
+    fontWeight: 800,
+  },
+  input: {
+    color: 'black',
+  },
 }
 
 export default function AuthenticationTools({ type }: { type: string }) {
   const [open, setOpen] = React.useState(false)
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
   const { loading, isAuthenticated, logIn, logOut } = useAuth()
+
+  React.useEffect(() => {
+    console.log(email)
+  }, [email])
 
   const gotoProfile = () => {
     navigate(routes.profile())
@@ -46,7 +72,7 @@ export default function AuthenticationTools({ type }: { type: string }) {
               borderColor: 'black',
               borderWidth: '1px',
             }}
-            className="py-2 px-4 text-black hover:bg-black hover:text-white transition duration-100 rounded"
+            className="py-2 px-4 bg-black text-white transition duration-100 rounded"
           >
             <p className="Manrope600">Sign out</p>
           </button>
@@ -132,7 +158,7 @@ export default function AuthenticationTools({ type }: { type: string }) {
               borderColor: 'black',
               borderWidth: '1px',
             }}
-            className="py-2 px-4 text-black hover:bg-black hover:text-white transition duration-100 rounded"
+            className="py-2 px-4  bg-black text-white transition duration-100 rounded"
           >
             <p className="Manrope600">Sign in</p>
           </button>
@@ -141,7 +167,6 @@ export default function AuthenticationTools({ type }: { type: string }) {
           <button
             onClick={async () => {
               await logOut()
-
               navigate('/')
             }}
             style={{
@@ -183,17 +208,134 @@ export default function AuthenticationTools({ type }: { type: string }) {
           >
             <Fade in={open}>
               <Box
-                className="group block max-w-xs rounded-lg bg-white ring-1 ring-slate-900/5 shadow-lg"
+                className="rounded-lg p-10 bg-white ring-1 ring-slate-900/5 shadow-lg"
                 sx={style}
               >
-                <button
-                  onClick={async () => {
-                    await logIn()
-                    handleClose()
-                  }}
-                >
-                  Sign in with google
-                </button>
+                <Grid spacing={3} container style={{ width: '100%' }}>
+                  <Grid item xs={12}>
+                    <p className="Manrope500" style={{ fontSize: '15px' }}>
+                      Welcome back
+                    </p>
+                    <p className="Manrope800" style={{ fontSize: '25px' }}>
+                      Sign In
+                    </p>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <input
+                      placeholder="email"
+                      type="email"
+                      onChange={(text) => setEmail(text.target.value)}
+                      className="Manrope800"
+                      style={{
+                        width: '100%',
+                        borderWidth: '2px',
+                        borderColor: 'black',
+                        color: 'black',
+                        borderRadius: '12.5px',
+                        padding: '5px',
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <input
+                      type="password"
+                      placeholder="password"
+                      onChange={(text) => setPassword(text.target.value)}
+                      className="Manrope800"
+                      style={{
+                        width: '100%',
+                        borderWidth: '2px',
+                        borderColor: 'black',
+                        color: 'black',
+                        borderRadius: '12.5px',
+                        padding: '5px',
+                      }}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <button
+                      style={{
+                        width: '100%',
+                        backgroundColor: '#007AFF',
+                        color: 'white',
+                        height: '40px',
+                        borderRadius: '12.5px',
+                      }}
+                      onClick={async () => {
+                        console.log(`
+                          email: ${email},
+                          password: ${password},
+                        `)
+                        await logIn({
+                          email: email,
+                          password: password,
+                        })
+                      }}
+                    >
+                      <p className="Manrope800" style={{ color: 'white' }}>
+                        Continue
+                      </p>
+                    </button>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <p
+                      className="Manrope700"
+                      style={{ fontSize: '12.5px', textAlign: 'center' }}
+                    >
+                      or continue with
+                    </p>
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <div
+                      style={{
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderRadius: '12.5px',
+                        display: 'flex',
+                        height: '40px',
+                        backgroundColor: 'black',
+                      }}
+                      onClick={async () => {
+                        await logIn()
+                        handleClose()
+                      }}
+                    >
+                      <GoogleIcon sx={{ color: 'white' }} />
+                    </div>
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <div
+                      style={{
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderRadius: '12.5px',
+                        display: 'flex',
+                        height: '40px',
+                        backgroundColor: 'black',
+                      }}
+                      onClick={async () => {
+                        await logIn()
+                        handleClose()
+                      }}
+                    >
+                      <FacebookRoundedIcon sx={{ color: 'white' }} />
+                    </div>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <p
+                      className="Manrope700"
+                      style={{ fontSize: '12.5px', textAlign: 'center' }}
+                    >
+                      don’t have an account?
+                    </p>
+                  </Grid>
+                </Grid>
               </Box>
             </Fade>
           </div>

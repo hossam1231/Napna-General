7b52889529c79117a5bcc55316d8c54feeb869a7
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2022-12-03 08:23:53 +0000

    init

--- prettier.config.js
+++ prettier.config.js
@@ -1,18 +1,5 @@
-// https://prettier.io/docs/en/options.html
-/** @type {import('prettier').RequiredOptions} */
 module.exports = {
-  trailingComma: 'es5',
-  bracketSpacing: true,
-  tabWidth: 2,
-  semi: false,
   singleQuote: true,
-  arrowParens: 'always',
-  overrides: [
-    {
-      files: 'Routes.*',
-      options: {
-        printWidth: 999,
-      },
-    },
-  ],
+  semi: false,
+  plugins: [require('prettier-plugin-tailwindcss')],
 }
